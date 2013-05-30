5f67075246da80f548abf8e7b87ea7f46f799b55
yihua.huang <code4crafter@gmail.com>
2013-05-30 14:37:37 +0800

    fix readme

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ WAR4E_URL=http://code4craft.qiniudn.com/war4e.jar
 WAR_URL=http://code4craft.qiniudn.com/hostd-server-0.1.0.war
 curl http://code4craft.github.io/blackhole/install.sh | sh
 mkdir -p ${HOSTD_HOME}
+cd ${HOSTD_HOME}
 curl ${WAR4E_URL} > war4e.jar
 curl ${WAR_URL} > hostd-server-0.1.0.war
 curl http://code4craft.github.io/hostd/zonesfile.db > zonesfile.db
