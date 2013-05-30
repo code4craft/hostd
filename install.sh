c9406610c859c99d586b5b918d7383eabd13bfee
yihua.huang <code4crafter@gmail.com>
2013-05-30 12:57:41 +0800

    sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+HOME_DIR=/usr/local/blackhole3/
+BLACK_HOLE_URL=http://42.96.139.35/download/blackhole-1.2.0.tar.gz
+mkdir -p ${HOME_DIR}
+cd ${HOME_DIR}
+echo "start to download ${BLACK_HOLE_URL}"
+curl ${BLACK_HOLE_URL} > blackhole-1.2.0.tar.gz
+tar -xzf blackhole-1.2.0.tar.gz
+rm -f blackhole-1.2.0.tar.gz
